9edf955e2a43367b35b0a119f90bf9707af469de
raschetov58 <raschetov58@gmail.com>
2025-04-29 14:38:23 +0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env: {},
+    viewportHeight: 1920,
+    viewportWidth: 1080,
+    baseUrl: 'https://login.qa.studio/',
+  },
+};
